507ee483824bf4ba19fa13a1a0ef0a7ab8c874cd
Paul Meaney <Paul@pauls-air.nuigalway.ie>
2016-09-29 16:59:17 +0100

    new

---
+++ src/main/java/part1/Student.java
@@ -0,0 +1,17 @@
+package part1;
+
+public class Student {
+	
+	private String name, username, dob, age, id;
+
+	public Student(String name, String username, String dob, String age, String id) {
+		super();
+		this.name = name;
+		this.username = username;
+		this.dob = dob;
+		this.age = age;
+		this.id = id;
+	}
+	
+
+}
